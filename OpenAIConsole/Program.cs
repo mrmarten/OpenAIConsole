2d5aa506516389d38434fa83b7745e3d322dce2b
Marten Sjo <martensjo5@hotmail.com>
2021-05-03 22:10:57 +0200

    Add files via upload

---
+++ OpenAIConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OpenAI_API;
+
+namespace OpenAIConsole
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("sk-Calling API");
+            var api = new OpenAI_API.OpenAIAPI(new APIAuthentication("sk-"),engine: Engine.Davinci);
+;
+
+            var result = await api.Completions.CreateCompletionAsync("how many people live on earth?");
+            Console.WriteLine(result.ToString());
+            // should print something that starts with "Three"
+        }
+    }
+}
